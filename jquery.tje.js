ace8500123d11ea66f0d7b3fd3ef2d60e15cc8b1
kurtextrem <kurtextrem@gmail.com>
2011-03-17 22:49:16 +0100

    Müde, gehe nun xD

--- jquery.tje.js
+++ jquery.tje.js
@@ -1,18 +1,39 @@
 (function( $ ){
 
-	$.fn.tooltip = function( options ) {
+	$.fn.TJE = function( options ) {
 
 		var default_settings = {
-			'big': true,
-			'italic': true
+			big: true,
+			italic: true
 		};
 
-		return this.each(function() {
-			if ( options ) {
-				$.extend( default_settings, options );
-			}
+		var html = "<div class='tinyjseditor'><div class='toolbar'>";
 
+		if ( options ) {
+			$.extend( default_settings, options );
+		}
+
+		if(default_settings.big){
+			html += "<button class='tje_button tje_b'>B</button>";
+		}
+
+		if(default_settings.italic){
+			html += "<button class='tje_button tje_i'>I</button>";
+		}
+		html += this;
+		html += "</div></div>";
+
+		// append to DOM
+		this.prepend(html);
+		// remove the old textarea
+		this.remove();
+
+		// register button clicks
+		$('.tje_button').click(function(){
+			$(html).find('textarea').val(this.val()+'iksde');
 		});
+
+		return html;
 
 	};
 })( jQuery );
