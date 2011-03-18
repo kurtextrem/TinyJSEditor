3322bd5e06c4be165dbdd1384d3f15885d131d08
kurtextrem <kurtextrem@gmail.com>
2011-03-18 08:47:26 +0100

    Example is now there!

--- jquery.tje.js
+++ jquery.tje.js
@@ -7,7 +7,8 @@
 			italic: true
 		};
 
-		var html = "<div class='tinyjseditor'><div class='toolbar'>";
+		var html = "<div class='tinyjseditor' id='"+this.attr('id')+"'><div class='toolbar'>";
+		this.attr('id', ''); // delete id from textfield
 
 		if ( options ) {
 			$.extend( default_settings, options );
@@ -20,20 +21,20 @@
 		if(default_settings.italic){
 			html += "<button class='tje_button tje_i'>I</button>";
 		}
-		html += this;
-		html += "</div></div>";
+
+		html += '</div></div>'; // close toolbar div
+		html = $(html); // make it jQuery object
 
 		// append to DOM
-		this.prepend(html);
-		// remove the old textarea
-		this.remove();
+		this.before(html);
+		this.appendTo(html.attr('id'));
 
 		// register button clicks
 		$('.tje_button').click(function(){
-			$(html).find('textarea').val(this.val()+'iksde');
+			$(html.attr('id')).find('textarea').val(this.val()+'iksde');
 		});
 
-		return html;
+		return $(html);
 
 	};
 })( jQuery );
