a0d1ba2d892d60f00e19f319a6fab058df1a03f2
kurtextrem <kurtextrem@gmail.com>
2011-03-19 13:57:54 +0100

    Changes

--- jquery.tje.js
+++ jquery.tje.js
@@ -1,17 +1,35 @@
 (function( $ ){
 
-	$.fn.TJE = function( options ) {
+	$.fn.TJE = function( options, replace ) {
+
+		var template = "<div class='tinyjseditor'><div class='toolbar'><button class='tje_button tje_b'>B</button><button class='tje_button tje_i'>I</button><input type='range' min='0' max='1'><br></div><textarea>{textarea_value}</textarea><div id='wysiwyg' contenteditable>{textarea_value_compiled}</div></div>";
 
 		var default_settings = {
 			big: true,
-			italic: true
+			italic: true,
+			strike: true,
+			overline: true,
+			underline: false,
+			switcher: true
 		};
+
+		var default_replace = {
+			'[b]': '<strong>',
+			'[i]': '<i>', // yes, <i> is allowed in HTML5, and <b> too!
+			'[s]': '<del>', // instead of <s>, cause this isnt allowed in html5 xD
+			'[o]': '<span style="text-decoration: overline;">', // there is no element for this
+			'[u]': '<span style="text-decoration: underline;">'
+		}
 
 		var html = "<div class='tinyjseditor' id='"+this.attr('id')+"'><div class='toolbar'>";
 		this.attr('id', ''); // delete id from textfield
 
 		if ( options ) {
 			$.extend( default_settings, options );
+		}
+
+		if (replacement){
+			$.extend(default_replace, replace);
 		}
 
 		if(default_settings.big){
